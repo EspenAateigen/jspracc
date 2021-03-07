75443eea3cc9f6f00852b56553132c53f59212ac
EspenAateigen <Espenaateigen@gmail.com>
2021-03-07 16:35:47 +0100

    added js
    lots of js

---
+++ js/script1.js
@@ -0,0 +1,242 @@
+// Question 1
+var fabric = "wool";
+var threads = 15;
+var isSoft = true;
+
+// Question 2
+var firstName = "Espen";
+var lastName = "Aateigen";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+// Question 3
+var animal = "frog";
+var typeOfAnimal = typeof animal;
+console.log("The type of frog is:", typeOfAnimal);
+
+// Question 4
+var orderHasShipped = false;
+if (orderHasShipped === true) {
+  console.log("The order shipped");
+} else console.log("The order did not ship");
+
+// Question 5
+for (var i = 7; i <= 13; i++) {
+  console.log(i);
+}
+
+/*
+// Question 1
+var integer = 5;
+var decimal = 7.3;
+
+console.log(integer);
+console.log(decimal);
+
+// Question 2
+var workingDays =
+  "Monday" +
+  "-" +
+  "Tuesday" +
+  "-" +
+  "Wednesday" +
+  "-" +
+  "Thursday" +
+  "-" +
+  "Friday";
+
+console.log(workingDays);
+
+// Question 3
+var addition = 5 + 5;
+var subtraction = 4 - 2;
+var multiplication = 2 * 3;
+var division = 6 / 3;
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+
+// Question 4
+var loggedIn = false;
+
+console.log(loggedIn);
+
+// Question 5
+var amount = "The frogs are angry.";
+var lengthOfString = amount.length;
+
+console.log(amount);
+console.log(lengthOfString);
+
+// Question 6
+var timeElapsed = "timeElapsed";
+var totalPrice = "totalPrice";
+
+console.log(timeElapsed);
+console.log(totalPrice);
+
+// Question 7
+var number1 = "53";
+var number2 = "44.6";
+
+var convertedInteger = parseInt(number1);
+var convertedDecimalNumber = parseFloat(number2);
+
+console.log(convertedInteger);
+console.log(convertedDecimalNumber);
+
+// Question 8
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeLeft;
+
+console.log(pet);
+console.log(age);
+console.log(discountApplied);
+console.log(timeLeft);
+
+var typeOfPet = typeof pet;
+var typeOfAge = typeof age;
+var typeOfDiscountApplied = typeof discountApplied;
+var typeOfTimeLeft = typeof timeLeft;
+
+console.log(typeOfPet);
+console.log(typeOfAge);
+console.log(typeOfDiscountApplied);
+console.log(typeOfTimeLeft);
+
+// Question 9
+var headingText = "Updating heading";
+var heading1 = document.querySelector("h1");
+heading1.innerHTML = headingText;
+
+console.log("heading1:", heading1.innerHTML);
+
+// Question 10
+var bodyColor = document.querySelector("body");
+bodyColor.style.backgroundColor = "lightskyblue";
+
+console.log(bodyColor.style.backgroundColor);
+
+// Question 1
+var firstNumber = 34;
+var secondNumber = 89;
+
+if (firstNumber < secondNumber) {
+  console.log("The first number is smaller than the second");
+} else {
+  console.log("The first number is greater than the second");
+}
+
+// Question 2
+var age = 11;
+var minimunAge = "13";
+
+if (age >= minimunAge) {
+  console.log("Old enough");
+} else {
+  console.log("Not old enough");
+}
+
+// Question 3
+var income = "11.050";
+var maximumIncome = "13.075";
+
+if (parseFloat(income) <= parseFloat(maximumIncome)) {
+  console.log("Income accepted");
+} else {
+  console.log("Income too high");
+}
+
+// Question 4
+var studentAge = 14;
+
+if (studentAge >= 6 && studentAge <= 13) {
+  console.log("Barneskole");
+} else if (studentAge >= 14 && studentAge <= 16) {
+  console.log("Ungdomsskole");
+} else if (studentAge >= 17 && studentAge <= 19) {
+  console.log("Videregående skole");
+} else console.log("Invalid age");
+
+// Question 5
+var color = "blue";
+
+if (color === "orange") {
+  console.log("This color is a bit rubbish");
+}
+
+// Question 6
+var invoicePaid = false;
+
+if (invoicePaid === false) {
+  console.log("Not paid");
+}
+
+// Question 7
+var selectedNumber = 11;
+var winningNumber = "15";
+
+if (selectedNumber !== parseInt(winningNumber)) {
+  console.log("You loose again!");
+}
+
+// Question 8
+var dayOfTheWeek = 2;
+var dayName;
+
+switch (dayOfTheWeek) {
+  case 1:
+    dayName = "Monday";
+    break;
+
+  case 2:
+    dayName = "Tuesday";
+    break;
+
+  case 3:
+    dayName = "Wednesday";
+    break;
+
+  case 4:
+    dayName = "Thursday";
+    break;
+
+  case 5:
+    dayName = "Friday";
+    break;
+
+  case 6:
+    dayName = "Saturday";
+    break;
+
+  case 7:
+    dayName = "Sunday";
+    break;
+
+  default:
+    dayName = "Invalid day number";
+}
+
+console.log(dayName);
+
+// Question 1
+for (var i = 0; i <= 7; i++) {
+  console.log(i);
+}
+
+// Question 2
+for (var o = 10; o > 0; o--) {
+  console.log(o);
+}
+
+// Question 3
+for (var p = 10; p <= 20; p++)
+  if (p % 2 !== 0) {
+    console.log(p);
+  }
+*/
